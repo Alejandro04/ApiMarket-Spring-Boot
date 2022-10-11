2d358739ca9b5ef37dc6f50fd0724e3e3a1a77c8
Alejandro Roa <alejodev04@gmail.com>
2022-10-11 12:08:10 -0400

    Agregar ProductRepository

--- src/main/java/com/apispring/apimarket/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/apispring/apimarket/persistence/crud/ProductCrudRepository.java
@@ -1,7 +1,15 @@
 package com.apispring.apimarket.persistence.crud;
 
 import com.apispring.apimarket.persistence.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+
+    // we can use
+    // @Query(value="SELECT * FROM products WHERE id_category = ?", nativeQuery = true)
+    List<Product> findByIdCategory(int idCategory);
+    List<Product> findByIdCategoryOrderByNameAsc(int idCategory);
 }
--- src/main/java/com/apispring/apimarket/persistence/crud/ProductRepository.java
+++ src/main/java/com/apispring/apimarket/persistence/crud/ProductRepository.java
@@ -1,14 +1,37 @@
 package com.apispring.apimarket.persistence.crud;
 
 import com.apispring.apimarket.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductRepository {
 
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll(){
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory(int idCategory){
+        return productCrudRepository.findByIdCategory(idCategory);
+    }
+
+    public List<Product> getByCategoryOrderByNameAsc(int idCategory){
+        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    }
+
+    public Optional<Product> getProductById(int idProduct){
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product saveProduct(Product product){
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int idProduct){
+        productCrudRepository.deleteById(idProduct);
     }
 }
